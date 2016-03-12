c3dcc59a9929837db2fa3c1bdb5ec7dda399f01e
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-03-12 11:06:32 -0800

    Builds collection of found colors.

--- js/main.js
+++ js/main.js
@@ -17,10 +17,32 @@ window.onload = function() {
     context.imageSmoothingEnabled = false;
     context.drawImage(image, 0, 0, width, height);
 
+    colors = {}
+
     for (var x = 0; x < image.width; x++) {
       for (var y = 0; y < image.height; y++) {
-        console.log(context.getImageData(x * ratio, y * ratio, 1, 1).data);
+        var colorData = context.getImageData(x * ratio, y * ratio, 1, 1).data;
+        var r = colorData[0],
+            g = colorData[1],
+            b = colorData[2],
+            a = colorData[3];
+
+        if (a) {
+          if (!(r in colors)) {
+            colors[r] = {}
+          }
+          if (!(g in colors[r])) {
+            colors[r][g] = {}
+          }
+          if (!(b in colors[r][g])) {
+            colors[r][g][b] = []
+          }
+
+          colors[r][g][b].push([x, y]);
+        }
       }
     }
+
+    console.log(colors);
   }
 }

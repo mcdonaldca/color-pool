41b2759a09c47682e3e96fd334d729a232633525
Caitlin McDonald <caitlin.adele.mcdonald@gmail.com>
2016-03-12 15:57:23 -0800

    Merging scales!

--- js/pool.js
+++ js/pool.js
@@ -5,8 +5,8 @@ function Pool() {
   this.displayContext = this.displayCanvas.getContext("2d");
   this.manipContext = this.manipCanvas.getContext("2d");
 
-  this.ratio = 3;
-  this.setImageWithSrc("img/test2.png");
+  this.ratio = 10;
+  this.setImageWithSrc("img/test.png");
 
   this.colorClickStack = [];
 
@@ -38,7 +38,10 @@ Pool.prototype.addClick = function(colorEl) {
 Pool.prototype.setImageWithSrc = function(imageSrc) {
   this.content.empty();
   var image = new Image();
-  image.onload = this.drawImageOnLoad();
+  var pool = this;
+  image.onload = function() {
+    pool.drawImageOnLoad(this);
+  }
   image.src = imageSrc;
 };
 
@@ -55,66 +58,61 @@ Pool.prototype.setOriginalImageDimensions = function(width, height) {
   this.imgHeight = height;
 }
 
-Pool.prototype.drawImageOnLoad = function() {
-  var pool = this;
+Pool.prototype.drawImageOnLoad = function(image) {
+  this.setOriginalImageDimensions(image.width, image.height);
+  this.manipCanvas.width = image.width;
+  this.manipCanvas.height = image.height;
+  this.displayCanvas.width = image.width * this.ratio;
+  this.displayCanvas.height = image.height * this.ratio;
 
-  return function() {
-    var image = this;
+  this.manipContext.imageSmoothingEnabled = false;
+  this.manipContext.drawImage(image, 0, 0);
 
-    pool.setOriginalImageDimensions(image.width, image.height);
-    pool.manipCanvas.width = image.width;
-    pool.manipCanvas.height = image.height;
-    pool.displayCanvas.width = image.width * pool.ratio;
-    pool.displayCanvas.height = image.height * pool.ratio;
+  colors = {};
 
-    pool.manipContext.imageSmoothingEnabled = false;
-    pool.manipContext.drawImage(image, 0, 0);
+  var manipData = this.manipContext.getImageData(0, 0, image.width, image.height).data;
+  for (var i = 0; i < manipData.length; i += 4) {
 
-    colors = {};
+    var r = manipData[i],
+        g = manipData[i + 1],
+        b = manipData[i + 2],
+        a = manipData[i + 3];
 
-    var manipData = pool.manipContext.getImageData(0, 0, image.width, image.height).data;
-    for (var i = 0; i < manipData.length; i += 4) {
-
-      var r = manipData[i],
-          g = manipData[i + 1],
-          b = manipData[i + 2],
-          a = manipData[i + 3];
-
-      if (a != "0") {
-        if (!(r in colors)) {
-          colors[r] = {}
-        }
-        if (!(g in colors[r])) {
-          colors[r][g] = {}
-        }
-        if (!(b in colors[r][g])) {
-          var colorEl = document.createElement("div");
-          colorEl.className = "color " + r + "-" + g + "-" + b;
-          colorEl.setAttribute("r", r);
-          colorEl.setAttribute("g", g);
-          colorEl.setAttribute("b", b);
-          colorEl.setAttribute("a", a / 255);
-          colorEl.style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + (a / 255).toString() + ")";
-          pool.content.append(colorEl);
-
-          colors[r][g][b] = new Color(colorEl, r, g, b, a);
-        }
-
-        var y = Math.floor(i / (image.width * 4));
-        var x = i - (y * image.width * 4) / 4;
-        colors[r][g][b].addLocation(x, y);
+    if (a != "0") {
+      if (!(r in colors)) {
+        colors[r] = {}
       }
+      if (!(g in colors[r])) {
+        colors[r][g] = {}
+      }
+      if (!(b in colors[r][g])) {
+        var colorEl = document.createElement("div");
+        colorEl.className = "color " + r + "-" + g + "-" + b;
+        colorEl.setAttribute("r", r);
+        colorEl.setAttribute("g", g);
+        colorEl.setAttribute("b", b);
+        colorEl.setAttribute("a", a / 255);
+        colorEl.style.backgroundColor = "rgba(" + r + ", " + g + ", " + b + ", " + (a / 255).toString() + ")";
+        this.content.append(colorEl);
+
+        colors[r][g][b] = new Color(colorEl, r, g, b, a);
+      }
+
+      var y = Math.floor(i / (image.width * 4));
+      var x = (i - (y * image.width * 4)) / 4;
+      colors[r][g][b].addLocation(x, y);
     }
+  }
 
-    pool.setColors(colors);
-    $(".color").click(function() {
-      pool.addClick(this);
-    });
+  this.setColors(colors);
+  var pool = this;
+  $(".color").click(function() {
+    pool.addClick(this);
+  });
 
-    pool.displayContext.imageSmoothingEnabled = false;
-    pool.displayContext.scale(pool.ratio, pool.ratio);
-    pool.displayContext.drawImage(pool.manipCanvas, 0, 0);
-  };
+  this.displayContext.imageSmoothingEnabled = false;
+  this.displayContext.scale(this.ratio, this.ratio);
+  this.displayContext.drawImage(this.manipCanvas, 0, 0);
 };
 
 Pool.prototype.mergeColors = function() {
@@ -123,14 +121,14 @@ Pool.prototype.mergeColors = function() {
     var mergeFrom = this.colorClickStack[stackHeight - 1];
     var mergeTo = this.colorClickStack[stackHeight - 2];
 
-    var imageData = this.displayContext.getImageData(0, 0, this.displayCanvas.width, this.displayCanvas.height);
+    var imageData = this.manipContext.getImageData(0, 0, this.imgWidth, this.imgHeight);
     var data = imageData.data;
 
     for (var i = 0; i < mergeFrom.locations.length; i++) {
       var x = mergeFrom.locations[i][0],
           y = mergeFrom.locations[i][1];
 
-      var calcIndex = (x * this.ratio * 4) + (4 * this.imgWidth * this.ratio * this.ratio * y);
+      var calcIndex = (x * 4) + (4 * this.imgWidth * y);
 
       data[calcIndex] = mergeTo.r;
       data[calcIndex + 1] = mergeTo.g;
@@ -141,10 +139,15 @@ Pool.prototype.mergeColors = function() {
     }
     this.colors[mergeFrom.r][mergeFrom.g][mergeFrom.b] = undefined;
     var target = "." + mergeFrom.r + "-" + mergeFrom.g + "-" + mergeFrom.b;
-    $(target).remove();
+    var el = $(target).remove()[0];
+    el.className +=  " merged";
+    this.content.append(el);
     
+    this.manipContext.imageSmoothingEnabled = false;
+    this.manipContext.putImageData(imageData, 0, 0);
+
     this.displayContext.imageSmoothingEnabled = false;
-    this.displayContext.putImageData(imageData, 0, 0);
+    this.displayContext.drawImage(this.manipCanvas, 0, 0);
 
   } else {
     console.log("not enough colors clicked");
